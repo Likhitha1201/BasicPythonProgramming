1c1ae5a709948685fdab36d97f7a5c93958d3696
likhithashylu <likitha.shylu@gmail.com>
2024-09-23 16:27:44 +0530

    [LIKHITHA] add- Applying logic to fetch the first and last colour from a list

---
+++ Dispfirstlast.py
@@ -0,0 +1,30 @@
+""" 
+
+    @Author: Likhitha S
+    @Date: 23-09-2024 16:50
+    @Last Modified by: Likhitha S
+    @Last Modified time: 23-09-2024 16:50
+    @Title: Write a Python program to display the first and last colors from a list.
+
+"""
+
+def main():
+    """
+        
+        Description: 
+            This function is used fetch first and last color from a list.
+        Parameters:
+          colours from the user .
+        Return:
+            It returns the first and last colour from the list. 
+        
+    """
+    colours=input("Enter the colours separated by(,): ")
+    #colours=['red','green','purple','black','white']--
+    colour=colours.split(',')
+    print("First color will be: ",colour[0].strip())
+    print("Last color will be: ",colour[len(colour)-1].strip())
+    
+    
+if __name__=="__main__":
+    main()
