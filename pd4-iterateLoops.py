5a6c69e2f87b178ca2f42b9f9b6bd210aae8cd30
likhithashylu <likitha.shylu@gmail.com>
2024-09-26 15:30:12 +0530

    [LIKHITHA] add- Applying logic to traverse using looping statements.

--- pd4-iterateLoops.py
+++ pd4-iterateLoops.py
@@ -4,12 +4,22 @@
     @Date: 26-09-2024 14:40
     @Last Modified by: Likhitha S
     @Last Modified time: 26-09-2024 14:40
-    @Title: Write a Python script to concatenate following dictionaries to create a new one.
+    @Title: Write a Python program to iterate over dictionaries using for loops.
     
 """
 
 
 def looping(dict):
+    """
+        
+        Description: 
+            This function is used to iterate using looping statement.
+        Parameters:
+            dist is an parameter given from calling function 
+        Return:
+            It prints the new dictionary. 
+    
+    """
     print('---Using ForLoop---')
     for index,value in dict.items():
         print(index,value)
@@ -25,14 +35,7 @@ def looping(dict):
         
 def main():
     """
-        
-        Description: 
-            This function is used to concatenate different dictionaries into single dictionary.
-        Parameters:
-            dist1, dict2, dict3 is an parameter given from calling function 
-        Return:
-            It prints the new dictionary. 
-    
+        Here, the function that is used will be looping statements.
     """
     
     dict={1:'liki', 2:'rachuu', 3:'menni', 4:'chitti'}
