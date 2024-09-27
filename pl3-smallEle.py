ca8b4d419dcc31e1a727d5267b8769ef4819b74b
likhithashylu <likitha.shylu@gmail.com>
2024-09-27 15:20:57 +0530

    [LIKHITHA] add-Applying logic to print's the smallest ele present in a list.

---
+++ pl3-smallEle.py
@@ -0,0 +1,31 @@
+""" 
+
+    @Author: Likhitha S
+    @Date: 27-09-2024 15:15
+    @Last Modified by: Likhitha S
+    @Last Modified time: 27-09-2024 15:15
+    @Title: Write a Python program to find smallest ele from a list..
+    
+"""
+
+   
+def main():
+    """
+        
+        Description:
+            This function to finad smallest ele from a list.
+        Parameters:
+           lt is a container which holds the data init .
+        Return:
+            It print's smallest element from a list .
+                    
+    """
+    
+    lt=[12,34,45,56,57]
+    small=min(lt)
+        
+    print("smallest ele in a list is: ",small)
+    
+    
+if __name__=="__main__":
+    main()
