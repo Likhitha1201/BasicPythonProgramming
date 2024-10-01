bac64c804f2de4d7f82f5ce0687bc15f2bce21ac
likhithashylu <likitha.shylu@gmail.com>
2024-10-01 10:55:38 +0530

    [LIKHITHA] add-Applying logic to calculate the length of the given string.

---
+++ sp1-LengthStr.py
@@ -0,0 +1,29 @@
+""" 
+
+    @Author: Likhitha S
+    @Date: 30-09-2024 10:55
+    @Last Modified by: Likhitha S
+    @Last Modified time: 1-10-2024 10:55
+    @Title:Write a Python program to calculate the length of a string.
+    
+"""
+  
+
+def main():
+    """
+        
+        Description:
+            This function is used to calculate the length of the given string.
+        Parameter:
+            s1 is container which is used to store the string type of data.
+        Return:
+           It print's the length of the given string.
+                    
+    """
+    
+    s1="Rachuu"
+    print(f"length of the given {s1} is: {len(s1)}")
+    
+    
+if __name__=="__main__":
+    main()
